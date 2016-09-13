9102af1087ec213ef4ba15708819c0de8c2ac2e0
Matt Richard <matt@richardsoftware.com>
2016-09-13 13:19:37 -0500

    Add some docs and doc placeholders
    Most of these functions need a TODO: get a more 'elmy' name above them.

--- src/DisjointSet/Computation.elm
+++ src/DisjointSet/Computation.elm
@@ -1,22 +1,43 @@
-module Computation exposing (Computation, find, union, return, andThen, eval, eval', mapM, sequence, map)
+module DisjointSet.Computation exposing (Computation, find, union, return, andThen, eval, eval', mapM, sequence)
+{-| Composable "Computation" objects that handle threading the updated DisjointSet object for you.
+
+# Definition
+@docs Computation, find, union, eval, eval', return
+
+# Composition
+@docs andThen
+
+# List Helpers
+@docs mapM, sequence
+
+
+-}
 
 import DisjointSet as DSet exposing (DisjointSet)
 
+{-| Represents a series of operations over a DisjointSet object producing a result of type a.
+That is, a `Computation Int` produces an `Int` when evaluated.
 
+To actually evaluate a computation, see the [`eval`](#eval) function.
+
+-}
 type Computation a
     = State (DisjointSet -> ( a, DisjointSet ))
 
 
+{-| -}
 find : Int -> Computation Int
 find id =
     State (DSet.find id)
 
 
+{-| -}
 union : Int -> Int -> Computation ()
 union a b =
     State <| \set -> ( (), DSet.union a b set )
 
 
+{-| -}
 andThen : Computation a -> (a -> Computation b) -> Computation b
 andThen (State s) k =
     State
@@ -31,22 +52,25 @@ andThen (State s) k =
                 k' set'
 
 
+{-| -}
 return : a -> Computation a
 return a =
     State <| \set -> ( a, set )
 
 
+{-| -}
 eval : Computation a -> DisjointSet -> ( a, DisjointSet )
 eval (State s) set =
     s set
 
 
+{-| -}
 eval' : DisjointSet -> Computation a -> ( a, DisjointSet )
 eval' =
     (flip eval)
 
 
-
+{-| -}
 sequence : List (Computation a) -> Computation (List a)
 sequence =
     let
@@ -58,11 +82,9 @@ sequence =
         List.foldr k (return [])
 
 
+{-| -}
 mapM : (a -> Computation b) -> List a -> Computation (List b)
 mapM k =
     sequence << List.map k
 
 
-map : (a -> b) -> Computation a -> Computation b
-map f a =
-    a `andThen` (f >> return)
