397ebf1bd059d4644fac9dabde812f526a77b611
Matt Richard <matt@richardsoftware.com>
2016-10-16 18:04:29 -0500

    Very quick pass on doc strings.  They still need work.

--- src/DisjointSet/Computation.elm
+++ src/DisjointSet/Computation.elm
@@ -1,4 +1,4 @@
-module DisjointSet.Computation exposing (Computation, find, union, return, andThen, eval,  mapM, sequence, map)
+module DisjointSet.Computation exposing (Computation, find, union, return, andThen, eval, mapM, sequence, map)
 
 {-| Composable "Computation" objects that handle threading the updated DisjointSet object for you.
 
@@ -26,19 +26,19 @@ type Computation a
     = State (DisjointSet -> ( a, DisjointSet ))
 
 
-{-| -}
+{-| Create a computation that finds which subset the given id's set belongs to. -}
 find : Int -> Computation Int
 find id =
     State (DSet.find id)
 
 
-{-| -}
+{-| Create a computation that unions the two given sets -}
 union : Int -> Int -> Computation ()
 union a b =
     State <| \set -> ( (), DSet.union a b set )
 
 
-{-| -}
+{-| Chain computations together where the second may depend on the result of the first -}
 andThen : (a -> Computation b) -> Computation a -> Computation b
 andThen callback (State fa) =
     State
@@ -53,19 +53,19 @@ andThen callback (State fa) =
                 k newSet
 
 
-{-| -}
+{-| Create a computation that always returns the value given.  Frequently used to end an `andThen` chain. -}
 return : a -> Computation a
 return a =
     State <| \set -> ( a, set )
 
 
-{-| -}
+{-| Run a computation -}
 eval : DisjointSet -> Computation a -> ( a, DisjointSet )
 eval set (State s) =
     s set
 
 
-{-| -}
+{-| Create a computation that runs each of a list of computations in sequence -}
 sequence : List (Computation a) -> Computation (List a)
 sequence =
     List.foldr (map2 (::)) (return [])
@@ -77,13 +77,14 @@ mapM k =
     sequence << List.map k
 
 
-{-| -}
+{-| Transform the result of a computation -}
 map : (a -> b) -> Computation a -> Computation b
 map f a =
     a |> andThen (f >> return)
 
 
-{-| -}
+-- I think I'm going to leave this internal for now because I can't think of any
+-- reason you should need it where `andThen` wouldn't be more clear anyway
 map2 : (a -> b -> c) -> Computation a -> Computation b -> Computation c
 map2 f (State fa) (State fb) =
     -- Part of me hates implementing this at this level since it technically
